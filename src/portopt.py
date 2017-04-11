f772a12aca45752f3552a4aaa3e9f4e820e1be24
Jingwei Wu <jw7nm@virginia.edu>
2017-04-11 08:40:06 -0400

    Remove log config

--- src/portopt.py
+++ src/portopt.py
@@ -8,8 +8,6 @@ import numpy as np
 from scipy.optimize import minimize
 import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s  %(name)s  %(levelname)s  %(message)s')
 logger = logging.getLogger(__name__)
 
 class PortOpt:
