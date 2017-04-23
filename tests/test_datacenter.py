b97c853f39c385f04dec896b0fa3ba2f50feed03
Jingwei Wu <jw7nm@virginia.edu>
2017-04-22 22:13:00 -0400

    Add logging

--- tests/test_datacenter.py
+++ tests/test_datacenter.py
@@ -1,4 +1,7 @@
 import src.datacenter as dc
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(name)s  %(levelname)s  %(message)s')
 
 d = dc.DataCenter()
 df = d.load_codes_return(['000001', '000002'], '2010-01-01', '2010-02-01')
