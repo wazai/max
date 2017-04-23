786718fea6649502fffbd3bc969d8513e475b525
Jingwei Wu <jw7nm@virginia.edu>
2017-04-23 09:37:24 -0400

    change utils to a place holder

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,2 @@
-def date_10to8(date):
-    return date[:4] + date[5:7] + date[8:10]
-
-def date_8to10(date):
-    return date[:4] + '-' + date[4:6] + '-' + date[6:8]
+# place holder
+# put utility functions here
