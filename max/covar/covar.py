a7245fa6dd0bb31a56a4f40149c098783eb1076a
Jingwei Wu <jw7nm@virginia.edu>
2017-04-29 16:27:28 -0400

    Treat date as index, add log

--- max/covar/covar.py
+++ max/covar/covar.py
@@ -12,15 +12,16 @@ class Covar(object):
 
     def __init__(self, df=pd.DataFrame(), window=90):
         logger.info('Initializing Covariance base class')
-        if 'date' not in df.columns:
-            raise Exception('date column is needed in input data frame')
+        if type(df.index) != pd.tseries.index.DatetimeIndex:
+            raise Exception('type of index must be pandas time series')
         if 'code' not in df.columns:
             raise Exception('code column is needed in input data frame')
         if 'return' not in df.columns:
             raise Exception('return column is needed in input data frame')
         self.name = 'Ex Post'
-        self.return_ = df.pivot(index='date', columns='code', values='return')
-        logger.info('Time period from %s to %s', min(df.date).strftime('%Y-%m-%d'), max(df.date).strftime('%Y-%m-%d'))
+        self.return_ = df.pivot(columns='code', values='return')
+        self.n_dates, self.n_stocks = self.return_.shape
+        logger.info('Time period from %s to %s', min(df.index).strftime('%Y-%m-%d'), max(df.index).strftime('%Y-%m-%d'))
         logger.info('# of dates: %i, # of stocks: %i', self.return_.shape[0], self.return_.shape[1])
         logger.info('Rolling window %i days', window)
         self.window = window
@@ -34,22 +35,23 @@ class Covar(object):
         self.ex_post['cov'] = np.array([util.ex_post_cov(self.return_, self.window, x) for x in self.return_.index])
 
     def to_csv(self, cov, folder):
+        logger.info('Saving files to csv')
         path = os.path.join(DataCenter.get_path('covariance'), folder)
         cols = self.return_.columns
-        n_dates = self.return_.shape[0]
-        for i in range(self.window, n_dates):
+        for i in range(self.window, self.n_dates):
             d = pd.DataFrame(cov[i, :, :], columns=cols)
             d['code'] = cols
             d = d[['code']+cols.tolist()]
             date = self.return_.index[i]
-            dirname = os.path.join(path, str(date.year))
-            if not os.path.isdir(dirname):
-                os.makedirs(dirname)
-            filename = os.path.join(dirname, date.strftime('%Y%m%d')+'.csv')
+            dir_name = os.path.join(path, str(date.year))
+            if not os.path.isdir(dir_name):
+                os.makedirs(dir_name)
+            filename = os.path.join(dir_name, date.strftime('%Y%m%d')+'.csv')
             logger.info('Saving file to %s', filename)
             d.to_csv(filename, index=False)
 
     def plot_ex_post(self, variable, i, j=None):
+        logger.info('Plotting ex-post %s', variable)
         if variable not in ['cov', 'cor', 'vol']:
             raise Exception('variable not valid')
         if variable == 'vol':
@@ -59,4 +61,4 @@ class Covar(object):
                 raise Exception('Need to provide both i and j for cor/cov plot')
             else:
                 ex_post_series = self.ex_post[variable][self.window:, i, j]
-        util.plot_series(ex_post_series, index=self.return_.index[self.window:], y_label=variable, legend=[self.name])
+        util.plot_series(ex_post_series, index=self.return_.index[self.window:], y_label=variable, legend=['Ex Post'])
