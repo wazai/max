fa7fa4bcd51e1f4947ccf00067ef897a1e487f02
Jingwei Wu <jw7nm@virginia.edu>
2017-05-29 10:54:15 -0400

    add benchmark and frequency as default option in strategy

--- max/strategy.py
+++ max/strategy.py
@@ -15,7 +15,7 @@ class Strategy(object):
     @param alpha: Alpha object
     @param port: Portfolio object, contains covariance matrix
     @param name: string
-    @param option: dict, including rebalance frequency etc
+    @param option: dict, including benchmark (default sh) and rebalance frequency (default 1 day)
     """
 
     def __init__(self, rule, alpha=None, port=None, name='NoOne', option=None):
@@ -27,4 +27,11 @@ class Strategy(object):
             raise Exception('universe of alpha and portfolio does not match')
         self.universe = port.universe
         self.name = name
-        self.option = dict() if option is None else option
+        if option is None:
+            option = {'benchmark': 'sh', 'frequency': 1}
+        else:
+            if 'benchmark' not in option.keys():
+                option['benchmark'] = 'sh'
+            if 'frequency' not in option.keys():
+                option['frequency'] = 1
+        self.option = option
